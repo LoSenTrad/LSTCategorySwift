7c0fb6ef9343c88e29a2f827874fadad3a920934
LoSenTrad <losentrad@163.com>
2021-05-11 11:49:09 +0800

    0.1.0

---
+++ LSTCategorySwift/Classes/LSTString.swift
@@ -0,0 +1,17 @@
+//
+//  LSTString.swift
+//  LSTCategory
+//
+//  Created by LoSenTrad on 2021/4/29.
+//
+
+import Foundation
+
+
+extension String  {
+    
+    public func stringToURL() -> URL {
+        return URL.init(string: self)!
+    }
+    
+}
