b842be9ffc5ea804216affe52bd40b7cb0cce1ac
LoSenTrad <losentrad@163.com>
2021-06-03 09:15:35 +0800

    - other

--- LSTCategorySwift/Classes/LSTString.swift
+++ LSTCategorySwift/Classes/LSTString.swift
@@ -10,10 +10,11 @@ import Foundation
 
 extension String  {
     
+    /// String 转 URL
     public func lst_stringToURL() -> URL? {
         return URL.init(string: self)
     }
-    
+    /// 获取文本宽度
     public func lst_getWidth(font: UIFont) -> CGFloat {
         let lab = UILabel.init(frame: CGRect(x: 0, y: 0, width: 1000, height: 0))
         lab.text = self
@@ -21,7 +22,7 @@ extension String  {
         lab.sizeToFit()
         return lab.frame.size.width
     }
-    
+    /// 获取文本高度
     public func lst_getHeight(font: UIFont, maxWidth: CGFloat) -> CGFloat {
         let lab = UILabel.init(frame: CGRect(x: 0, y: 0, width: maxWidth, height: 0))
         lab.text = self
